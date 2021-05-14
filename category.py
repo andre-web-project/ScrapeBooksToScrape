f3ff215013b125863cd8f1b79ba5a2792dd8a632
Cedrik-Web <cedrik.rougier@icloud.com>
2021-05-14 17:18:49 +0100

    correction bug dans les categories, creation code pour le cas de recherche de tout les livres , creation code pour le cas recherche de pages multiples dans une categorie choisis

--- category.py
+++ category.py
@@ -19,31 +19,35 @@ if reponse.ok:
         a = li.find('a')
         category = a.text
         category = re.sub('[\n]', '', category)
-        categories = category.strip()
-        choixCategory.append(categories)
+        categorySansEspace = category.strip()
+        categoriesSansMajuscule = categorySansEspace.upper()
+        categorySansEspace = categoriesSansMajuscule.capitalize()
+        choixCategory.append(categorySansEspace)
         link = a['href']
         linkV = link[2:]
-        links[categories] = "https://books.toscrape.com/catalogue/category" + linkV
+        links[categorySansEspace] = "https://books.toscrape.com/catalogue/category" + linkV
 
+# suppression de la categorie Books qui n'en est pas une
+# del choixCategory[0]
 
 # fonction qui demande a utilisateur de choisir une ou toutes les catégorie
 def demandeDeCategories():
     print(choixCategory)
     print("")
-    choix = input("choisit ta categories parmit la listes si dessus : " )
+    choix = input("choisis ta categories parmit la listes si dessus ou Books pour tout selectionner : " )
     choixstr = str(choix)
     choixstr.lower()
-    categorie = choixstr.title()
+    categorie = choixstr.capitalize()
     if categorie == "":
         print("Erreur !")
-        print("Vous devez choisir une catégorie ou écrire Books pour tout sélectioner.")
+        print("Vous devez choisir une catégorie ou écrire Books pour tout sélectionner.")
         return demandeDeCategories()
     elif categorie in links:
         liens = links.get(categorie)
         return liens, categorie
     else:
         print("Erreur !")
-        print("Vous devez choisir une catégorie ou écrire Books pour tout sélectioner.")
+        print("Vous devez choisir une catégorie ou écrire Books pour tout sélectionner.")
         return demandeDeCategories()
 
 
@@ -56,44 +60,97 @@ response = requests.get(newUrls)
 
 # fonction permettant de scrapper la totalité des livres d'une catégorie
 if reponse.ok:
-    soup = BeautifulSoup(response.text, 'lxml')
     links = []
-    liste = soup.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
-    nomberBooks = soup.find('form', {'class': 'form-horizontal'}).find('strong').text
-    if int(nomberBooks) > 20:
-        pages = soup.find('li', {'class': 'next'}).find('a')
-        pages2 = pages['href']
-        pageNext = url[:-10]
-        link = (pageNext + pages2)
-        reponses = requests.get(link)
-        soups = BeautifulSoup(reponses.text, 'lxml')
-        liste2 = soups.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
-        for li in liste2:
-            a2 = li.find('a')
-            x2 = a2["href"]
-            x2 = re.sub('[..]', '', x2)
-            y2 = x2[2:]
-            link2 = y2[:-4]
-            links.append('https://books.toscrape.com/catalogue' + link2 + '.html')
-        for li in liste:
-            a = li.find('a')
-            x = a["href"]
-            x = re.sub('[..]', '', x)
-            y = x[2:]
-            link = y[:-4]
-            links.append('https://books.toscrape.com/catalogue' + link + '.html')
+# rajout de la fonction Books pour recuperer la totalité de livres----------
+    if retourCategorie == 'Books':
+        soup = BeautifulSoup(reponse.text, "lxml")
+        page = soup.find('li', {'class': 'next'}).find('a')
+        nombrePage = soup.find('li', {'class': 'current'}).text
+        pageSansEspace = nombrePage.strip()
+        totalPage = pageSansEspace[10:]
+        pageTotal = int(totalPage) - 1
+        urlPageNext = page['href']
+        books = []
+        newUrl = "https://books.toscrape.com/catalogue/category/books_1/" + urlPageNext
+        books.append(newUrls)
+        books.append(newUrl)
+        for i in range(pageTotal):
+            newUrl = "https://books.toscrape.com/catalogue/category/books_1/" + urlPageNext
+            newResponse = requests.get(newUrl)
+            soups = BeautifulSoup(newResponse.text, "lxml")
+            pages = soups.find('li', {'class': 'next'}).find('a')
+            urlPageNext = pages['href']
+            newUrl = "https://books.toscrape.com/catalogue/category/books_1/" + urlPageNext
+            books.append(newUrl)
+        for l in books:
+            r = requests.get(l)
+            s = BeautifulSoup(r.text, "lxml")
+            liste = s.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
+            for li in liste:
+                a2 = li.find('a')
+                x2 = a2["href"]
+                x2 = re.sub('[..]', '', x2)
+                y2 = x2[2:]
+                link2 = y2[:-4]
+                links.append('https://books.toscrape.com/catalogue' + link2 + '.html')
         print('il y a : ' + str(len(links)) + ' livres dans cette catégorie.')
         print('Un fichier csv avec ces données à etait générer')
+# fin du rajout de code ---------------------------------------------------------------
     else:
-        for li in liste:
-            a = li.find('a')
-            x = a["href"]
-            x = re.sub('[..]', '', x)
-            y = x[2:]
-            link = y[:-4]
-            links.append('https://books.toscrape.com/catalogue' + link + '.html')
-        print('il y a : ' + str(len(links)) + ' livres dans cette catégorie.')
-        print('Un fichier csv avec ces données à etait générer')
+        soup = BeautifulSoup(response.text, 'lxml')
+        liste = soup.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
+        nomberBooks = soup.find('form', {'class': 'form-horizontal'}).find('strong').text
+        if int(nomberBooks) > 20:
+            pages = soup.find('li', {'class': 'next'}).find('a')
+            pages2 = pages['href']
+            pageNext = url[:-10]
+            liens = (pageNext + pages2)
+            reponses = requests.get(liens)
+            soups = BeautifulSoup(reponses.text, 'lxml')
+            liste2 = soups.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
+        # rajout du code pageNext -------------------------------------------
+            page = soup.find('li', {'class': 'next'}).find('a')
+            nombrePage = soup.find('li', {'class': 'current'}).text
+            pageSansEspace = nombrePage.strip()
+            totalPage = pageSansEspace[10:]
+            pageTotal = int(totalPage) -2
+            urlPageNext = page['href']
+            newUrl2 = newUrls[:-10]
+            link = []
+            link.append(newUrls)
+            link.append(newUrl2 + urlPageNext)
+            for i in range(pageTotal):
+                newUrl = newUrl2 + urlPageNext
+                newResponse = requests.get(newUrl)
+                soups = BeautifulSoup(newResponse.text, "lxml")
+                pages = soups.find('li', {'class': 'next'}).find('a')
+                urlPageNext = pages['href']
+                newUrl = newUrl2 + urlPageNext
+                link.append(newUrl)
+            for l in link:
+                r = requests.get(l)
+                s = BeautifulSoup(r.text, "lxml")
+                liste1 = s.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
+                for li in liste1:
+                    a2 = li.find('a')
+                    x2 = a2["href"]
+                    x2 = re.sub('[..]', '', x2)
+                    y2 = x2[2:]
+                    link2 = y2[:-4]
+                    links.append('https://books.toscrape.com/catalogue' + link2 + '.html')
+            # fin du code rajouter ---------------------------------------------------------
+            print('il y a : ' + str(len(links)) + ' livres dans cette catégorie.')
+            print('Un fichier csv avec ces données à etait générer')
+        else:
+            for li in liste:
+                a = li.find('a')
+                x = a["href"]
+                x = re.sub('[..]', '', x)
+                y = x[2:]
+                link = y[:-4]
+                links.append('https://books.toscrape.com/catalogue' + link + '.html')
+            print('il y a : ' + str(len(links)) + ' livres dans cette catégorie.')
+            print('Un fichier csv avec ces données à etait générer')
 
 
 # ligne necessaires pour la mise en page du fichier csv
@@ -110,6 +167,3 @@ with codecs.open('scrappingCategories' + retourCategorie + '.csv', 'w', encoding
     for row in links:
         ligne = retourCategorie+ ';' + row + '\n'
         file.write(ligne)
-
-
-
