43d92151679736a2456f9d1585bdaa555dad22dd
Cedrik-Web <cedrik.rougier@icloud.com>
2021-05-15 13:42:33 +0100

    Creation fonction scrapper categorie , creation de fonction recherche des livres dans une catégorie , creation fonction d'inpression de resultat de la recherches livres par categories

--- category.py
+++ category.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import codecs
 import re
 
+
 # creation de url de base avec ca requette sur la page home du site
 url ="https://books.toscrape.com/catalogue/category/books_1/index.html"
 reponse = requests.get(url)
@@ -27,11 +28,10 @@ if reponse.ok:
         linkV = link[2:]
         links[categorySansEspace] = "https://books.toscrape.com/catalogue/category" + linkV
 
-# suppression de la categorie Books qui n'en est pas une
-# del choixCategory[0]
 
-# fonction qui demande a utilisateur de choisir une ou toutes les catégorie
+# fonction qui demande a utilisateur de choisir une ou toutes les catégorie prend en charge des cas d'erreurs
 def demandeDeCategories():
+    print("")
     print(choixCategory)
     print("")
     choix = input("choisis ta categories parmit la listes si dessus ou Books pour tout selectionner : " )
@@ -51,119 +51,118 @@ def demandeDeCategories():
         return demandeDeCategories()
 
 
-# variable de recuperation de valeurs necessaires pour les autres fonction
-retourFonction = demandeDeCategories()
-newUrls = retourFonction[0]
-retourCategorie = retourFonction[1]
-response = requests.get(newUrls)
-
-
-# fonction permettant de scrapper la totalité des livres d'une catégorie
-if reponse.ok:
-    links = []
-# rajout de la fonction Books pour recuperer la totalité de livres----------
-    if retourCategorie == 'Books':
-        soup = BeautifulSoup(reponse.text, "lxml")
-        page = soup.find('li', {'class': 'next'}).find('a')
-        nombrePage = soup.find('li', {'class': 'current'}).text
-        pageSansEspace = nombrePage.strip()
-        totalPage = pageSansEspace[10:]
-        pageTotal = int(totalPage) - 1
-        urlPageNext = page['href']
-        books = []
-        newUrl = "https://books.toscrape.com/catalogue/category/books_1/" + urlPageNext
-        books.append(newUrls)
-        books.append(newUrl)
-        for i in range(pageTotal):
-            newUrl = "https://books.toscrape.com/catalogue/category/books_1/" + urlPageNext
-            newResponse = requests.get(newUrl)
-            soups = BeautifulSoup(newResponse.text, "lxml")
-            pages = soups.find('li', {'class': 'next'}).find('a')
-            urlPageNext = pages['href']
-            newUrl = "https://books.toscrape.com/catalogue/category/books_1/" + urlPageNext
-            books.append(newUrl)
-        for l in books:
-            r = requests.get(l)
-            s = BeautifulSoup(r.text, "lxml")
-            liste = s.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
-            for li in liste:
-                a2 = li.find('a')
-                x2 = a2["href"]
-                x2 = re.sub('[..]', '', x2)
-                y2 = x2[2:]
-                link2 = y2[:-4]
-                links.append('https://books.toscrape.com/catalogue' + link2 + '.html')
-        print('il y a : ' + str(len(links)) + ' livres dans cette catégorie.')
-        print('Un fichier csv avec ces données à etait générer')
-# fin du rajout de code ---------------------------------------------------------------
-    else:
-        soup = BeautifulSoup(response.text, 'lxml')
-        liste = soup.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
-        nomberBooks = soup.find('form', {'class': 'form-horizontal'}).find('strong').text
-        if int(nomberBooks) > 20:
-            pages = soup.find('li', {'class': 'next'}).find('a')
-            pages2 = pages['href']
-            pageNext = url[:-10]
-            liens = (pageNext + pages2)
-            reponses = requests.get(liens)
-            soups = BeautifulSoup(reponses.text, 'lxml')
-            liste2 = soups.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
-        # rajout du code pageNext -------------------------------------------
+# creation fonction scrapper selection
+def scrappingSelectionDemander():
+    # variable de recuperation des valeurs necessaires pour la suite du code
+    retourFonction = demandeDeCategories()
+    newUrls = retourFonction[0]
+    retourCategorie = retourFonction[1]
+    response = requests.get(newUrls)
+    # fonction permettant de scrapper la totalité des livres de la selection demandée
+    if reponse.ok:
+        links = []
+    # rajout de la fonction Books pour recuperer la totalité de livres----------
+        if retourCategorie == 'Books':
+            soup = BeautifulSoup(reponse.text, "lxml")
             page = soup.find('li', {'class': 'next'}).find('a')
             nombrePage = soup.find('li', {'class': 'current'}).text
             pageSansEspace = nombrePage.strip()
             totalPage = pageSansEspace[10:]
-            pageTotal = int(totalPage) -2
+            pageTotal = int(totalPage) - 2
             urlPageNext = page['href']
-            newUrl2 = newUrls[:-10]
-            link = []
-            link.append(newUrls)
-            link.append(newUrl2 + urlPageNext)
+            books = []
+            newUrl = "https://books.toscrape.com/catalogue/category/books_1/" + urlPageNext
+            books.append("https://books.toscrape.com/catalogue/category/books_1/index.html")
+            books.append(newUrl)
             for i in range(pageTotal):
-                newUrl = newUrl2 + urlPageNext
+                newUrl = "https://books.toscrape.com/catalogue/category/books_1/" + urlPageNext
                 newResponse = requests.get(newUrl)
                 soups = BeautifulSoup(newResponse.text, "lxml")
                 pages = soups.find('li', {'class': 'next'}).find('a')
                 urlPageNext = pages['href']
-                newUrl = newUrl2 + urlPageNext
-                link.append(newUrl)
-            for l in link:
+                newUrl = "https://books.toscrape.com/catalogue/category/books_1/" + urlPageNext
+                books.append(newUrl)
+            for l in books:
                 r = requests.get(l)
                 s = BeautifulSoup(r.text, "lxml")
-                liste1 = s.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
-                for li in liste1:
+                liste = s.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
+                for li in liste:
                     a2 = li.find('a')
                     x2 = a2["href"]
                     x2 = re.sub('[..]', '', x2)
                     y2 = x2[2:]
                     link2 = y2[:-4]
-                    links.append('https://books.toscrape.com/catalogue' + link2 + '.html')
-            # fin du code rajouter ---------------------------------------------------------
-            print('il y a : ' + str(len(links)) + ' livres dans cette catégorie.')
+                    links.append('https://books.toscrape.com/catalogue/' + link2 + '.html')
+            print("")
+            print('il y a : ' + str(len(links)) + ' livres au total sur le site.')
             print('Un fichier csv avec ces données à etait générer')
+    # fin du rajout de code ---------------------------------------------------------------
         else:
-            for li in liste:
-                a = li.find('a')
-                x = a["href"]
-                x = re.sub('[..]', '', x)
-                y = x[2:]
-                link = y[:-4]
-                links.append('https://books.toscrape.com/catalogue' + link + '.html')
-            print('il y a : ' + str(len(links)) + ' livres dans cette catégorie.')
-            print('Un fichier csv avec ces données à etait générer')
-
-
-# ligne necessaires pour la mise en page du fichier csv
-entetes = [
-    u'categorie',
-    u'liens du livre'
-]
-ligneEntete = ';'.join(entetes) + '\n'
-
-
-# code pour l'extraction du fichier csv de la categorie demander
-with codecs.open('scrappingCategories' + retourCategorie + '.csv', 'w', encoding='utf-8') as file:
-    file.write(ligneEntete)
-    for row in links:
-        ligne = retourCategorie+ ';' + row + '\n'
-        file.write(ligne)
+            soup = BeautifulSoup(response.text, 'lxml')
+            liste = soup.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
+            nomberBooks = soup.find('form', {'class': 'form-horizontal'}).find('strong').text
+            if int(nomberBooks) > 20:
+                pages = soup.find('li', {'class': 'next'}).find('a')
+                pages2 = pages['href']
+                pageNext = url[:-10]
+                liens = (pageNext + pages2)
+                reponses = requests.get(liens)
+                soups = BeautifulSoup(reponses.text, 'lxml')
+            # rajout du code pageNext -------------------------------------------
+                page = soup.find('li', {'class': 'next'}).find('a')
+                nombrePage = soup.find('li', {'class': 'current'}).text
+                pageSansEspace = nombrePage.strip()
+                totalPage = pageSansEspace[10:]
+                pageTotal = int(totalPage) -2
+                urlPageNext = page['href']
+                newUrl2 = newUrls[:-10]
+                link = []
+                link.append(newUrls)
+                link.append(newUrl2 + urlPageNext)
+                for i in range(pageTotal):
+                    newUrl = newUrl2 + urlPageNext
+                    newResponse = requests.get(newUrl)
+                    soups = BeautifulSoup(newResponse.text, "lxml")
+                    pages = soups.find('li', {'class': 'next'}).find('a')
+                    urlPageNext = pages['href']
+                    newUrl = newUrl2 + urlPageNext
+                    link.append(newUrl)
+                for l in link:
+                    r = requests.get(l)
+                    s = BeautifulSoup(r.text, "lxml")
+                    liste1 = s.findAll('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
+                    for li in liste1:
+                        a2 = li.find('a')
+                        x2 = a2["href"]
+                        x2 = re.sub('[..]', '', x2)
+                        y2 = x2[2:]
+                        link2 = y2[:-4]
+                        links.append('https://books.toscrape.com/catalogue' + link2 + '.html')
+                # fin du code rajouter ---------------------------------------------------------
+                print("")
+                print('il y a : ' + str(len(links)) + ' livres dans la selection: '+ retourCategorie)
+                print('Un fichier csv avec ces données à etait générer')
+            else:
+                for li in liste:
+                    a = li.find('a')
+                    x = a["href"]
+                    x = re.sub('[..]', '', x)
+                    y = x[2:]
+                    link = y[:-4]
+                    links.append('https://books.toscrape.com/catalogue' + link + '.html')
+                print("")
+                print('il y a : ' + str(len(links)) + ' livres dans la selection: '+ retourCategorie)
+                print('Un fichier csv avec ces données à etait générer')
+    # ligne necessaires pour la mise en page du fichier csv
+    entetes = [
+        u'Categorie',
+        u'Liens',
+    ]
+    ligneEntete = ";".join(entetes) + "\n"
+    # code pour l'extraction du fichier csv de la selection demandé
+    with codecs.open('scrappingCategories' + retourCategorie + '.csv', 'w', encoding='utf-8') as file:
+        file.write(ligneEntete)
+        for row in links:
+            ligne = retourCategorie + ';' + row + '\n'
+            file.write(ligne)
+    return links
--- main.py
+++ main.py
@@ -1,50 +1,82 @@
 import requests
 from bs4 import BeautifulSoup
 import codecs
+import os
+from category import scrappingSelectionDemander
 
-url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
-reponse = requests.get(url)
+# creation fonction recherchant les details du livre demandé
+def rechercheInfosBook():
+    links = scrappingSelectionDemander()
+    valeurs = []
+    for url in links:
+        urls = url
+        reponse = requests.get(urls)
+        if reponse.ok:
+            soup = BeautifulSoup(reponse.text, "lxml")
+            tds = soup.findAll('td')
+            title = soup.find("h1").text
+            price_including_taxs = tds[3].text
+            price_including_tax = price_including_taxs[1:]
+            price_excluding_taxs = tds[2].text
+            price_excluding_tax = price_excluding_taxs[1:]
+            number_available = tds[5].text
+            product_description = tds[1].text
+            category = soup.findAll('a')[3].text
+            review_rating = soup.find('p', class_='star-rating')['class'][1].lower()
+            liens = soup.find('img')['src']
+            liensImage = "https://books.toscrape.com/" + liens[6:]
+            image_url = liensImage
+            valeur = [title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url]
+            valeurs.append(valeur)
+    print("")
+    print("Les données des livres de la selection ont etait récuperer ainsi que les images de couvertures.")
+    return valeurs
 
-if reponse.ok:
-    soup = BeautifulSoup(reponse.text, "lxml")
 
-    tds = soup.findAll('td')
+def impressionDuDetails():
+    valeurs = rechercheInfosBook()
+    images = []
+    for i in valeurs:
+        image = i[-1]
+        images.append(image)
+    entre = input('Voulez creer un fichier avec ces données et enregistrer les images ? ')
+    validation = entre.lower()
+    entetes = [
+        u'Title',
+        u'price including tax',
+        u'price excluding tax',
+        u'number available',
+        u'product description',
+        u'category',
+        u'review rating',
+        u'image url'
+    ]
+    ligneEntete = ';'.join(entetes) + '\n'
+    # creation du dossier pour accueillir les images
+    if validation == "oui":
+        print("Le fichier csv a était généré.")
+        print("")
+        if not os.path.exists("imagesCouv"):
+            os.makedirs("imagesCouv")
+            for img in images:
+                nom = img.split("/")[-1]
+                dest = os.path.join("imagesCouv", nom)
+                contenu = i
+                if os.path.exists(dest):
+                    pass
+                else:
+                    with open(str(dest), "wb") as f:
+                        f.write(contenu)
+        with codecs.open('scrappingBooks.csv', 'w', encoding='utf-8') as file:
+            file.write(ligneEntete)
+            for row in valeurs:
+                ligne = ';'.join(row) + '\n'
+                file.write(ligne)
 
-    title = soup.find("h1").text
-    price_including_taxs = tds[3].text
-    price_including_tax = price_including_taxs[1:]
-    price_excluding_taxs = tds[2].text
-    price_excluding_tax = price_excluding_taxs[1:]
-    number_available = tds[5].text
-    product_description = tds[1].text
-    category = soup.findAll('a')[3].text
-    review_rating = soup.find('p', class_='star-rating')['class'][1].lower()
-    image_url = soup.find('img')['src']
+    else:
+        print("Fin de la recherche.")
+        print("")
 
-entetes = [
-    u'Title',
-    u'price including tax',
-    u'price excluding tax',
-    u'number available',
-    u'product description',
-    u'category',
-    u'review rating',
-    u'image url'
-]
-valeurs = [
-    title,
-    price_including_tax,
-    price_excluding_tax,
-    number_available,
-    product_description,
-    category,
-    review_rating,
-    image_url
-]
-ligneEntete = ';'.join(entetes) + '\n'
-ligne = ";".join(valeurs) + "\n\n"
 
-with codecs.open('scrappingBooks.csv', 'w', encoding='utf-8') as file:
-    file.write(ligneEntete)
-
+impressionDuDetails()
